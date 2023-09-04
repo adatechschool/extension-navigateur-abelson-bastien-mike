331787551bbe315cba95d9801186ff5dbfcfcab0
MikeMaille88 <mickael.bohrer@gmail.com>
2023-09-04 15:58:55 +0200

    test randomcolorify h1

--- background.js
+++ background.js
@@ -1,8 +1,21 @@
-// Écoute l'événement de clic sur l'icône de l'extension.
-chrome.browserAction.onClicked.addListener(function(tab) {
-	// Exécute le code pour modifier la couleur des titres h1.
+// Définition de la fonction getColor pour générer une couleur aléatoire.
+function getColor() {
+	return (
+	  "#" +
+	  Math.random()
+		.toString(16)
+		.slice(2, 8)
+	);
+  }
+  
+  // Écoute l'événement de clic sur l'icône de l'extension.
+  chrome.browserAction.onClicked.addListener(function(tab) {
+	// Récupère une couleur aléatoire en utilisant la fonction getColor.
+	var randomColor = getColor();
+	
+	// Exécute le code pour modifier la couleur des titres h1 avec la couleur aléatoire.
 	chrome.tabs.executeScript({
-	  code: 'document.querySelectorAll("h1").forEach(function(el) { el.style.color = "green"; });'
+	  code: `document.querySelectorAll("h1").forEach(function(el) { el.style.color = '${randomColor}'; });`
 	});
   });
   
