e94daa138b75707c293b13d010eb391a11fb4f6d
MikeMaille88 <mickael.bohrer@gmail.com>
2023-09-04 15:30:37 +0200

    test greenify h1

--- background.js
+++ background.js
@@ -1,12 +1,8 @@
-
-
-
-
-	chrome.browserAction.onClicked.addListener(function(tab) {
-	  
-	  chrome.tabs.executeScript({
-		code: ''
-	  });
-	  
-	  
+// Écoute l'événement de clic sur l'icône de l'extension.
+chrome.browserAction.onClicked.addListener(function(tab) {
+	// Exécute le code pour modifier la couleur des titres h1.
+	chrome.tabs.executeScript({
+	  code: 'document.querySelectorAll("h1").forEach(function(el) { el.style.color = "green"; });'
 	});
+  });
+  
